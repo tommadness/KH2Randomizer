715eab6dc1c3deb4489c086310c41473cba11388
tommadness <tom.m.cover@gmail.com>
2021-04-01 08:24:08 -0400

    Very basic webservice support.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from randomize import Randomize
-import flask
+from flask import Flask
+import flask as fl
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = Randomize(exclude = ["ExcludeFrom50","Atlantica","DataOrg","LingeringWill"], formExpMult={1:5,2:3,3:3,4:3,5:3})
+    return fl.send_file(
+        data,
+        mimetype='application/zip',
+        as_attachment=True,
+        attachment_filename='randoseed.zip'
+    )
+    
 Randomize(exclude = ["ExcludeFrom50","Atlantica","DataOrg","LingeringWill"], formExpMult={1:5,2:3,3:3,4:3,5:3})
