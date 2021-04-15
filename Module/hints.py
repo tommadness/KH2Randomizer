9aac971c8d2015ede3ba5c56070100331e0e6497
tommadness <tom.m.cover@gmail.com>
2021-04-15 12:53:38 -0400

    Add Second Chance and Once More to Shananas Hint pool

--- Module/hints.py
+++ Module/hints.py
@@ -1,4 +1,5 @@
 from List.configDict import itemType
+from Class.locationClass import KH2ItemStat
 
 class Hints:
     def generateHints(locationItems, hintsType):
@@ -8,7 +9,9 @@ class Hints:
             importantChecks = [itemType.FIRE, itemType.BLIZZARD, itemType.THUNDER, itemType.CURE, itemType.REFLECT, itemType.MAGNET, itemType.PROOF, itemType.PROOF_OF_CONNECTION, itemType.PROOF_OF_PEACE, itemType.FORM, itemType.TORN_PAGE, itemType.SUMMON]
             hintsText['world'] = {}
             for location,item in locationItems:
-                if item.ItemType in importantChecks:
+                if isinstance(location, KH2ItemStat):
+                    continue
+                if item.ItemType in importantChecks or item.Name == "Second Chance" or item.Name == "Once More":
                     if not location.LocationTypes[0] in hintsText['world']:
                         hintsText['world'][location.LocationTypes[0]] = []
                     hintsText['world'][location.LocationTypes[0]].append(item.Name)
