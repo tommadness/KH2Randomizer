4a632e838915528023d74095ed5e8f6af14dca6b
thundrio <12samn@gmail.com>
2021-08-28 16:19:15 -0700

    khbr update

--- Module/modifier.py
+++ Module/modifier.py
@@ -41,7 +41,6 @@ class SeedModifier():
         abilitylist = action + support
         abilitydict = {i.Name: i for i in abilitylist}
         possibleabilities = list(set([i.Name for i in abilitylist if i.Name not in ["Second Chance", "Once More"]]))
-        print(possibleabilities)
         randomabilitypool = []
         for _ in range(len(abilitylist)-2):
             choice = random.choice(possibleabilities)
