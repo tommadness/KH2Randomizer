2aa05bcec526552f965791bbf2b2998d0b87b1a0
DemonBoy <53849249+o0DemonBoy0o@users.noreply.github.com>
2021-09-02 03:40:18 -0500

    Added PC support
    Tested a method to include PC randomization support as well as added some missing menus to the list while commenting out some duplicates.
    Randomization for pc works, but requires on update for build_from_mm to properly patch the HD textures as currently it completely ignored the remaster3ed folder in the openkh/mod directory.

--- Module/randomCmdMenu.py
+++ Module/randomCmdMenu.py
@@ -12,41 +12,47 @@ class RandomCmdMenu():
             "ca0": "Port Royal",
             "dc0": "Disney Castle",
             "dc1": "Lingering Will",
-            "di0": "Destiny Island",
+            #"di0": "Destiny Island", #no visible difference from KH1
             "eh0": "The World That Never Was",
+            #"eh1": "The Dark Margin(?)", #looks like KH1 but with classic icons
+            #"es0": "Agrabah (alt), #for carpet minigames? need to check for differences.
             "hb0": "Hollow Bastion",
             "hb1": "Garden of Assemblage",
             "hb2": "Absent Silhouette",
             "he0": "Olympus Coliseum",
             "he1": "The Underworld",
             "lk0": "Pride Lands",
-            "lm0": "Atlantica",
+            "lm0": "Atlantica (PCSX2 Only)",
             "mu0": "Land of Dragons",
             "nm0": "Halloween Town",
             "nm1": "Christmas Town",
             "po0": "100 Acre Wood",
             "tr0": "Space Paranoids",
             "tt0": "Twilight Town",
+            "tt1": "Station of Calling", #looks like kh1 but with classic icons.
             "tt2": "Mysterious Tower",
             "tt3": "Mansion Basement",
             "tt4": "The White Room",
             "tt5": "Mansion",
             "tt6": "Betwixt & Between",
             "wi0": "Timeless River",
+            #"wm0": "World Map", #no visible difference from Betwixt & Between
             "zz0": "Kingdom Hearts 1"
             }
 
-    def randomizeCmdMenus(cmdMenuChoice, outZip, platform="PCSX2"):
-        if not platform == "PCSX2":
-            return ""
-        cmdMenus = [
+    def randomizeCmdMenus(cmdMenuChoice, outZip, platform):
+        #if not platform == "PCSX2":
+           #return ""
+        cmdMenusPCSX2 = [
             "al0",
             "bb0",
             "ca0",
             "dc0",
             "dc1",
-            "di0",
+            #"di0",
             "eh0",
+            #"eh1",
+            #"es0",
             "hb0",
             "hb1",
             "hb2",
@@ -60,15 +66,50 @@ class RandomCmdMenu():
             "po0",
             "tr0",
             "tt0",
+            "tt1",
             "tt2",
             "tt3",
             "tt4",
             "tt5",
             "tt6",
             "wi0",
-            "zz0"
-        ]
+            #"wm0",
+            "zz0"]
+        cmdMenusPC = [
+            "al0",
+            "bb0",
+            "ca0",
+            "dc0",
+            "dc1",
+            #"di0", is the only menu that uses a dds so it's easier to just leave out.
+            "eh0",
+            #"eh1",
+            #"es0",
+            "hb0",
+            "hb1",
+            "hb2",
+            "he0",
+            "he1",
+            "lk0",
+            #"lm0", #not fully in hd ports
+            "mu0",
+            "nm0",
+            "nm1",
+            "po0",
+            "tr0",
+            "tt0",
+            "tt2",
+            "tt3",
+            "tt4",
+            "tt5",
+            "tt6",
+            "wi0",
+            #"wm0",
+            "zz0"]
         cmdMenusDict = {}
+        cmdMenus = cmdMenusPCSX2
+        if platform == "PC":
+            cmdMenus = cmdMenusPC
         if cmdMenuChoice == "randAll":
             for cmdMenu in cmdMenus[:]:
                 cmdMenusDict[cmdMenu] = random.choice(cmdMenus)
@@ -81,23 +122,39 @@ class RandomCmdMenu():
             return ""
         else:
             for cmdMenu in cmdMenus[:]:
-                cmdMenusDict[cmdMenu] = cmdMenuChoice
+                if platform == "PC" and cmdMenuChoice == "lm0":
+                    cmdMenusDict[cmdMenu] = "zz0"
+                elif platform == "PC" and cmdMenuChoice == "di0":
+                    cmdMenusDict[cmdMenu] = "zz0"
+                else:
+                    cmdMenusDict[cmdMenu] = cmdMenuChoice
         cmdMenuAssets = []
-        region = ""
+        cmdMenuAssets2 = []
+        region = "us"
         if platform == "PCSX2":
             region = "jp"
         for key in cmdMenusDict:
-            cmdMenuAssets.append({
-                "name": "field2d\\jp\\{key}command.2dd".format(key=key),
-                "multi": [{"name": "field2d\\us\\{key}command.2dd".format(key=key)}],
-                "method": "copy",
-                "source": [{"name": "field2d\\{region}\\{cmdMenu}command.2dd".format(cmdMenu=cmdMenusDict[key], region=region), "type":"internal"}]
-            })
-
-        
+            if platform == "PCSX2":
+                cmdMenuAssets.append({
+                    "name": "field2d\\{region}\\{key}command.2dd".format(key=key, region=region),
+                    "multi": [{"name": "field2d\\us\\{key}command.2dd".format(key=key)}],
+                    "method": "copy",
+                    "source": [{"name": "field2d\\{region}\\{cmdMenu}command.2dd".format(cmdMenu=cmdMenusDict[key], region=region), "type":"internal"}]
+                })
+            if platform == "PC":
+                cmdMenuAssets.append({
+                    "name": "field2d\\{region}\\{key}command.2dd".format(key=key, region=region),
+                    "method": "copy",
+                    "source": [{"name": "field2d\\{region}\\{cmdMenu}command.2dd".format(cmdMenu=cmdMenusDict[key], region=region), "type":"internal"}]
+                }),
+                cmdMenuAssets2.append({
+                    "name": "remastered\\field2d\\{region}\\{key}command.2dd\\US_{key}command_2dd0.png".format(key=key, region=region),
+                    "method": "copy",
+                    "source": [{"name": "remastered\\field2d\\{region}\\{cmdMenu}command.2dd\\US_{cmdMenu}command_2dd0.png".format(cmdMenu=cmdMenusDict[key], region=region), "type":"internal"}]
+                })
+        if platform == "PC":
+            cmdMenuAssets = cmdMenuAssets + cmdMenuAssets2
+            
         return cmdMenuAssets
-
-
-
 
 
