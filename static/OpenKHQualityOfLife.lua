f66c72b1da5f904a532534bb491b273b7fc2acf3
zlittlefield <zlittlefield@gmail.com>
2021-09-21 11:18:13 -0400

    Updated QoL lua for 1.0.0.8

--- static/OpenKHQualityOfLife.lua
+++ static/OpenKHQualityOfLife.lua
@@ -7,7 +7,7 @@ if GAME_ID == 0xF266B00B or GAME_ID == 0xFAF99301 then
 	Btl0 = 0x1CE5D80
 elseif ENGINE_TYPE == 'BACKEND' then
 	Platform = 1
-	Now  = 0x0714DB8 - 0x56450E
+	Now  = 0x0714DB8 - 0x56454E
 	Save = 0x09A7070 - 0x56450E
 	Sys3 = 0x2A59DB0 - 0x56450E
 	Btl0 = 0x2A74840 - 0x56450E
