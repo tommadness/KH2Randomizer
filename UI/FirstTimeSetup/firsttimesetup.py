58495fcfb8171335d55393c0d21df8458d1fba8f
Thomas Cover <tom.m.cover@gmail.com>
2021-09-27 18:04:42 -0400

    Add pagination to First Time Setup

--- UI/FirstTimeSetup/firsttimesetup.py
+++ UI/FirstTimeSetup/firsttimesetup.py
@@ -3,7 +3,7 @@ import sys
 from PySide6.QtWidgets import (
     QMainWindow, QApplication,
     QLabel, QLineEdit, QPushButton, 
-    QTabWidget,QVBoxLayout,QHBoxLayout,QWidget
+    QTabWidget,QVBoxLayout,QHBoxLayout,QWidget,QStackedWidget
 )
 
 from PySide6.QtGui import QFont
@@ -15,17 +15,31 @@ class FirstTimeSetup(QMainWindow):
         w = 600
         h = 400
 
-        titleFont = QFont("MS Shell Dlg 2", 16)
-        titleFont.setBold(True)
+        self.titleFont = QFont("MS Shell Dlg 2", 16)
+        self.titleFont.setBold(True)
 
         self.setWindowTitle("First Time Setup")
         self.setFixedSize(w,h)
 
+        self.firstPage = self.firstPage()
+        self.pcsx2 = self.pcsx2Page()
+        self.pc = self.pcPage()
+
+
+        self.pages = QStackedWidget()
+
+        self.pages.addWidget(self.firstPage)
+        self.pages.addWidget(self.pcsx2)
+        self.pages.addWidget(self.pc)
+
+        self.setCentralWidget(self.pages)
+
+    def firstPage(self):
         mainLayout = QVBoxLayout()
         pageTitleLabel = QLabel()
         pageTitleLabel.setText("Welcome to Kingdom Hearts 2: Final Mix Randomizer")
         pageTitleLabel.setMaximumHeight(30)
-        pageTitleLabel.setFont(titleFont)
+        pageTitleLabel.setFont(self.titleFont)
 
         pageContentLabel = QLabel()
         pageContentLabel.setText("This will guide you through setting up the Kingdom Hearts 2: Final Mix randomizer.\nPlease select your game edition below:")
@@ -34,27 +48,30 @@ class FirstTimeSetup(QMainWindow):
 
         buttonWidget = QWidget()
         buttonLayout = QHBoxLayout()
+        
         pcsx2Button = QPushButton()
         pcsx2Button.setText("PCSX2")
+        pcsx2Button.clicked.connect(self.goToPcsx2Page)
+
         pcButton = QPushButton()
         pcButton.setText("PC")
+        pcButton.clicked.connect(self.goToPcPage)
 
         buttonLayout.addWidget(pcsx2Button)
         buttonLayout.addWidget(pcButton)
 
         buttonWidget.setLayout(buttonLayout)
         buttonWidget.setMaximumHeight(50)
-
-
-        widget = QWidget()
-
         mainLayout.addWidget(pageTitleLabel)
         mainLayout.addWidget(pageContentLabel)
         mainLayout.addWidget(buttonWidget)
 
+        widget = QWidget()
         widget.setLayout(mainLayout)
+        widget.setObjectName("firstPage")
+        return widget
 
-        self.setCentralWidget(widget)
+    
 
 
 
@@ -63,3 +80,26 @@ class FirstTimeSetup(QMainWindow):
         # configFile.write("Test")
         # configFile.close()
 
+    def pcsx2Page(self):
+        widget = QWidget()
+        layout = QVBoxLayout()
+        label = QLabel()
+        label.setText("PCSX2")
+        layout.addWidget(label)
+        widget.setLayout(layout)
+        return widget
+
+    def pcPage(self):
+        widget = QWidget()
+        layout = QVBoxLayout()
+        label = QLabel()
+        label.setText("PC")
+        layout.addWidget(label)
+        widget.setLayout(layout)
+        return widget
+
+    def goToPcsx2Page(self):
+        self.pages.setCurrentWidget(self.pcsx2)
+
+    def goToPcPage(self):
+        self.pages.setCurrentWidget(self.pc)
