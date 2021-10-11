fc0377b617e56400ec2a14ecf92e14a8add0a21b
zlittlefield <zlittlefield@gmail.com>
2021-10-11 12:53:24 -0400

    Saving work

--- UI/Submenus/HintsMenu.py
+++ UI/Submenus/HintsMenu.py
@@ -8,7 +8,7 @@ class HintsMenu(KH2Submenu):
         self.name="Hint Systems"
 
         hintSystem = QComboBox()
-        hintSystem.addItems(["Disabled","Shananas","JSmartee","JSmartee-FirstVisit","JSmartee-SecondVisit","JSmartee-FirstBoss","JSmartee-SecondBoss"])
+        hintSystem.addItems(["Disabled","Shananas","JSmartee","Points"])
         hintSystem.currentTextChanged.connect(self.changeHintsType)
         self.addOption("Hint System",hintSystem)
         self.addFlagOption(hintSystem,"hintsType")
@@ -16,8 +16,14 @@ class HintsMenu(KH2Submenu):
         self.noSelfHinting = QCheckBox()
         self.noSelfHinting.stateChanged.connect(lambda state : self.setKeyValue("preventSelfHinting",state==Qt.Checked))
         self.noSelfHinting.setCheckState(Qt.Checked)
-        self.addOption("Remove Self-Hinting Reports (JSmartee Only)",self.noSelfHinting)
+        self.addOption("Remove Self-Hinting Reports",self.noSelfHinting)
         self.addFlagOption(self.noSelfHinting,"preventSelfHinting")
+
+        self.pointReportProofs = QCheckBox()
+        self.pointReportProofs.stateChanged.connect(lambda state : self.setKeyValue("allowProofHinting",state==Qt.Checked))
+        self.pointReportProofs.setCheckState(Qt.Checked)
+        self.addOption("(Points Only) Reports can Hint Proofs",self.pointReportProofs)
+        self.addFlagOption(self.pointReportProofs,"allowProofHinting")
 
         # setting hints after to trigger the enable/disable of self-hinting
         hintSystem.setCurrentIndex(1)
@@ -26,11 +32,16 @@ class HintsMenu(KH2Submenu):
 
     def changeHintsType(self,text):
         self.setKeyValue("hintsType",text)
-        if "JSmartee" in text:
+        if "JSmartee" in text or "Points" in text:
             self.noSelfHinting.setEnabled(True)
         else:
             self.noSelfHinting.setEnabled(False)
+        if "Points" in text:
+            self.pointReportProofs.setEnabled(True)
+        else:
+            self.pointReportProofs.setEnabled(False)
 
     def updateWidgets(self):
         self.widgetList[0].setCurrentText(self.getKeyValue("hintsType"))
         self.widgetList[1].setCheckState(Qt.Checked if self.getKeyValue("preventSelfHinting") else Qt.Unchecked)
+        self.widgetList[2].setCheckState(Qt.Checked if self.getKeyValue("allowProofHinting") else Qt.Unchecked)
