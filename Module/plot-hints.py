37e7d9db0879480d67086821685036885954d3f8
thundrio <12samn@gmail.com>
2021-11-20 19:43:26 -0800

    statsanity option exposed to ui

---
+++ Module/plot-hints.py
@@ -0,0 +1,5 @@
+# hint in each org report
+# either tells you "nothing in this world" or important item is here
+# random, but always hints to proof locations
+# randomly picks a template of one of the reports from one of the games
+# tells you the world or the world and how (chest/etc)
