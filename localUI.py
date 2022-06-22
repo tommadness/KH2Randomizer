dbfbab2920ab5439c7327b60e681ab5f5fbebad3
ZakTheRobot <zlittlefield@gmail.com>
2022-06-22 19:32:21 -0400

    Bump version

--- localUI.py
+++ localUI.py
@@ -48,7 +48,7 @@ from UI.Submenus.SeedModMenu import SeedModMenu
 from UI.Submenus.SoraMenu import SoraMenu
 from UI.Submenus.StartingMenu import StartingMenu
 
-LOCAL_UI_VERSION = '2.1.1'
+LOCAL_UI_VERSION = '2.1.2'
 
 class Logger(object):
     def __init__(self, orig_stream):
