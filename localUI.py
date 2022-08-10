f382bcadc1268cca222d4b9dd7b0370ae77160ea
ZakTheRobot <zlittlefield@gmail.com>
2022-08-10 16:49:25 -0400

    Tagging 2.1.5

--- localUI.py
+++ localUI.py
@@ -50,7 +50,7 @@ from UI.Submenus.SeedModMenu import SeedModMenu
 from UI.Submenus.SoraMenu import SoraMenu
 from UI.Submenus.StartingMenu import StartingMenu
 
-LOCAL_UI_VERSION = '2.1.5-beta'
+LOCAL_UI_VERSION = '2.1.5'
 
 class Logger(object):
     def __init__(self, orig_stream):
