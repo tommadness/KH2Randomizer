19688af27cca7030775d3e164a2ae79351387fcf
ZakTheRobot <zlittlefield@gmail.com>
2022-09-11 15:37:44 -0400

    Version bump 2.1.6, restoring auto equip abilities

--- localUI.py
+++ localUI.py
@@ -51,7 +51,7 @@ from UI.Submenus.SeedModMenu import SeedModMenu
 from UI.Submenus.SoraMenu import SoraMenu
 from UI.Submenus.StartingMenu import StartingMenu
 
-LOCAL_UI_VERSION = '2.1.5'
+LOCAL_UI_VERSION = '2.1.6'
 
 class Logger(object):
     def __init__(self, orig_stream):
