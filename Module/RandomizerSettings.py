5f765a798356b7fa154a818724fcb5d212011de3
ZakTheRobot <zlittlefield@gmail.com>
2023-01-14 21:16:55 -0500

    Fixing prog hints issue

--- Module/RandomizerSettings.py
+++ Module/RandomizerSettings.py
@@ -253,7 +253,8 @@ class RandomizerSettings():
         self.progression_hint_settings["HintCosts"] = prog_points.get_hint_thresholds(num_worlds)
         self.progression_hint_settings["WorldCompleteBonus"] = [self.progression_world_complete_bonus]
         self.progression_hint_settings["ReportBonus"] = [self.progression_report_bonus]
-        self.progression_hint_settings["FinalXemnasReveal"] = [self.progression_reveal_all]
+        self.progression_hint_settings["FinalXemnasReveal"] = [1 if self.progression_reveal_all else 0]
+        print(self.progression_hint_settings["FinalXemnasReveal"])
 
 
         self.revealComplete = ui_settings.get(settingkey.REVEAL_COMPLETE)
