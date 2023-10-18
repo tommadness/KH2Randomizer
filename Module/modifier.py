8565a5d6ad342cf381425388b1dc86ad9d3f9c87
zaktherobot <zlittlefield@gmail.com>
2023-10-17 20:03:11 -0400

    Failing test for stat distributions

--- Module/modifier.py
+++ Module/modifier.py
@@ -9,9 +9,10 @@ from List.inventory.growth import GrowthAbility, GrowthType
 
 
 class SeedModifier:
-
     @staticmethod
-    def ability_list_modifier(option: AbilityPoolOption) -> Callable[[list[KH2Item], list[KH2Item]], list[KH2Item]]:
+    def ability_list_modifier(
+        option: AbilityPoolOption,
+    ) -> Callable[[list[KH2Item], list[KH2Item]], list[KH2Item]]:
         if option == AbilityPoolOption.DEFAULT:
             return SeedModifier.default_ability_pool
         elif option == AbilityPoolOption.RANDOMIZE:
@@ -24,16 +25,24 @@ class SeedModifier:
             raise SettingsException("Invalid ability pool option")
 
     @staticmethod
-    def default_ability_pool(action: list[KH2Item], support: list[KH2Item]) -> list[KH2Item]:
+    def default_ability_pool(
+        action: list[KH2Item], support: list[KH2Item]
+    ) -> list[KH2Item]:
         return action + support
 
     @staticmethod
-    def random_ability_pool(action: list[KH2Item], support: list[KH2Item]) -> list[KH2Item]:
+    def random_ability_pool(
+        action: list[KH2Item], support: list[KH2Item]
+    ) -> list[KH2Item]:
         randomizable_list = action + support
         randomizable_dict: dict[str, KH2Item] = {i.Name: i for i in randomizable_list}
-        possible_abilities = list(set(
-            i.Name for i in randomizable_list if i.Name not in [ability.SecondChance.name, ability.OnceMore.name]
-        ))
+        possible_abilities = list(
+            set(
+                i.Name
+                for i in randomizable_list
+                if i.Name not in [ability.SecondChance.name, ability.OnceMore.name]
+            )
+        )
         possible_abilities.sort()
         random_ability_pool = []
         for _ in range(len(randomizable_list) - 2):
@@ -49,12 +58,18 @@ class SeedModifier:
         return random_ability_pool
 
     @staticmethod
-    def random_support_ability_pool(action: list[KH2Item], support: list[KH2Item]) -> list[KH2Item]:
+    def random_support_ability_pool(
+        action: list[KH2Item], support: list[KH2Item]
+    ) -> list[KH2Item]:
         randomizable_list = support
         randomizable_dict: dict[str, KH2Item] = {i.Name: i for i in randomizable_list}
-        possible_abilities = list(set(
-            i.Name for i in randomizable_list if i.Name not in [ability.SecondChance.name, ability.OnceMore.name]
-        ))
+        possible_abilities = list(
+            set(
+                i.Name
+                for i in randomizable_list
+                if i.Name not in [ability.SecondChance.name, ability.OnceMore.name]
+            )
+        )
         possible_abilities.sort()
         random_ability_pool = []
         for _ in range(len(randomizable_list) - 2):
@@ -67,7 +82,9 @@ class SeedModifier:
         return random_ability_pool + action
 
     @staticmethod
-    def random_stackable_ability_pool(action: list[KH2Item], support: list[KH2Item]) -> list[KH2Item]:
+    def random_stackable_ability_pool(
+        action: list[KH2Item], support: list[KH2Item]
+    ) -> list[KH2Item]:
         stackable_abilities = [
             ability.ComboPlus.name,
             ability.AirComboPlus.name,
@@ -117,14 +134,31 @@ class SeedModifier:
             return [
                 (LevelUpStatBonus.STRENGTH, 2),
                 (LevelUpStatBonus.MAGIC, 2),
-                (LevelUpStatBonus.AP, 2)
+                (LevelUpStatBonus.AP, 2),
             ]
         else:
             return [
                 (LevelUpStatBonus.STRENGTH, 2),
                 (LevelUpStatBonus.MAGIC, 2),
                 (LevelUpStatBonus.DEFENSE, 1),
-                (LevelUpStatBonus.AP, 2)
+                (LevelUpStatBonus.AP, 2),
+            ]
+
+    @staticmethod
+    def level_up_stat_pool_weighted() -> list[tuple[LevelUpStatBonus, int]]:
+        return None
+        if glass_cannon:
+            return [
+                (LevelUpStatBonus.STRENGTH, 2),
+                (LevelUpStatBonus.MAGIC, 2),
+                (LevelUpStatBonus.AP, 2),
+            ]
+        else:
+            return [
+                (LevelUpStatBonus.STRENGTH, 2),
+                (LevelUpStatBonus.MAGIC, 2),
+                (LevelUpStatBonus.DEFENSE, 1),
+                (LevelUpStatBonus.AP, 2),
             ]
 
     @staticmethod
@@ -152,7 +186,9 @@ class SeedModifier:
         levels_by_growth_type: dict[GrowthType, int] = {}
         for chosen_growth_type in random.sample(options, k=num_growth_abilities):
             if chosen_growth_type in levels_by_growth_type:
-                levels_by_growth_type[chosen_growth_type] = levels_by_growth_type[chosen_growth_type] + 1
+                levels_by_growth_type[chosen_growth_type] = (
+                    levels_by_growth_type[chosen_growth_type] + 1
+                )
             else:
                 levels_by_growth_type[chosen_growth_type] = 1
 
--- tests/test_modifier.py
+++ tests/test_modifier.py
@@ -3,14 +3,32 @@ import unittest
 from List.ItemList import Items
 from List.configDict import StartingMovementOption, LevelUpStatBonus
 from List.inventory import ability
-from List.inventory.growth import HighJump1, HighJump2, HighJump3, HighJumpMax, QuickRun1, QuickRun2, \
-    QuickRun3, QuickRunMax, AerialDodge1, AerialDodge2, AerialDodge3, AerialDodgeMax, Glide1, Glide2, Glide3, \
-    GlideMax, DodgeRoll1, DodgeRoll2, DodgeRoll3, DodgeRollMax
+from List.inventory.growth import (
+    HighJump1,
+    HighJump2,
+    HighJump3,
+    HighJumpMax,
+    QuickRun1,
+    QuickRun2,
+    QuickRun3,
+    QuickRunMax,
+    AerialDodge1,
+    AerialDodge2,
+    AerialDodge3,
+    AerialDodgeMax,
+    Glide1,
+    Glide2,
+    Glide3,
+    GlideMax,
+    DodgeRoll1,
+    DodgeRoll2,
+    DodgeRoll3,
+    DodgeRollMax,
+)
 from Module.modifier import SeedModifier
 
 
 class Tests(unittest.TestCase):
-
     def test_default_ability_pool(self):
         all_action = Items.getActionAbilityList()
         all_support = Items.getSupportAbilityList() + Items.getLevelAbilityList()
@@ -21,9 +39,13 @@ class Tests(unittest.TestCase):
         all_action = Items.getActionAbilityList()
         all_support = Items.getSupportAbilityList() + Items.getLevelAbilityList()
         for _ in range(1000):
-            pool = SeedModifier.random_ability_pool(action=all_action, support=all_support)
+            pool = SeedModifier.random_ability_pool(
+                action=all_action, support=all_support
+            )
             self.assertEqual(len(pool), len(all_action) + len(all_support))
-            second_chance = next((a for a in pool if a.item == ability.SecondChance), None)
+            second_chance = next(
+                (a for a in pool if a.item == ability.SecondChance), None
+            )
             self.assertIsNotNone(second_chance)
             once_more = next((a for a in pool if a.item == ability.OnceMore), None)
             self.assertIsNotNone(once_more)
@@ -32,9 +54,13 @@ class Tests(unittest.TestCase):
         all_action = Items.getActionAbilityList()
         all_support = Items.getSupportAbilityList() + Items.getLevelAbilityList()
         for _ in range(1000):
-            pool = SeedModifier.random_support_ability_pool(action=all_action, support=all_support)
+            pool = SeedModifier.random_support_ability_pool(
+                action=all_action, support=all_support
+            )
             self.assertEqual(len(pool), len(all_action) + len(all_support))
-            second_chance = next((a for a in pool if a.item == ability.SecondChance), None)
+            second_chance = next(
+                (a for a in pool if a.item == ability.SecondChance), None
+            )
             self.assertIsNotNone(second_chance)
             once_more = next((a for a in pool if a.item == ability.OnceMore), None)
             self.assertIsNotNone(once_more)
@@ -45,9 +71,13 @@ class Tests(unittest.TestCase):
         all_action = Items.getActionAbilityList()
         all_support = Items.getSupportAbilityList() + Items.getLevelAbilityList()
         for _ in range(1000):
-            pool = SeedModifier.random_stackable_ability_pool(action=all_action, support=all_support)
+            pool = SeedModifier.random_stackable_ability_pool(
+                action=all_action, support=all_support
+            )
             self.assertEqual(len(pool), len(all_action) + len(all_support))
-            second_chance = next((a for a in pool if a.item == ability.SecondChance), None)
+            second_chance = next(
+                (a for a in pool if a.item == ability.SecondChance), None
+            )
             self.assertIsNotNone(second_chance)
             once_more = next((a for a in pool if a.item == ability.OnceMore), None)
             self.assertIsNotNone(once_more)
@@ -55,13 +85,33 @@ class Tests(unittest.TestCase):
     def test_level_up_stat_pool_normal(self):
         pool = SeedModifier.level_up_stat_pool(glass_cannon=False)
         bonuses = [bonus for bonus, _ in pool]
-        expected = [LevelUpStatBonus.STRENGTH, LevelUpStatBonus.MAGIC, LevelUpStatBonus.DEFENSE, LevelUpStatBonus.AP]
+        expected = [
+            LevelUpStatBonus.STRENGTH,
+            LevelUpStatBonus.MAGIC,
+            LevelUpStatBonus.DEFENSE,
+            LevelUpStatBonus.AP,
+        ]
         self.assertCountEqual(expected, bonuses)
 
     def test_level_up_stat_pool_glass_cannon(self):
         pool = SeedModifier.level_up_stat_pool(glass_cannon=True)
         bonuses = [bonus for bonus, _ in pool]
-        expected = [LevelUpStatBonus.STRENGTH, LevelUpStatBonus.MAGIC, LevelUpStatBonus.AP]
+        expected = [
+            LevelUpStatBonus.STRENGTH,
+            LevelUpStatBonus.MAGIC,
+            LevelUpStatBonus.AP,
+        ]
+        self.assertCountEqual(expected, bonuses)
+
+    def test_level_up_stat_pool_weighted_default(self):
+        pool = SeedModifier.level_up_stat_pool_weighted()
+        bonuses = [bonus for bonus, _ in pool]
+        expected = [
+            LevelUpStatBonus.STRENGTH,
+            LevelUpStatBonus.MAGIC,
+            LevelUpStatBonus.DEFENSE,
+            LevelUpStatBonus.AP,
+        ]
         self.assertCountEqual(expected, bonuses)
 
     def test_movement_disabled(self):
@@ -81,33 +131,63 @@ class Tests(unittest.TestCase):
 
     def test_movement_level_2(self):
         expected = [
-            HighJump1, HighJump2,
-            QuickRun1, QuickRun2,
-            AerialDodge1, AerialDodge2,
-            Glide1, Glide2,
-            DodgeRoll1, DodgeRoll2,
+            HighJump1,
+            HighJump2,
+            QuickRun1,
+            QuickRun2,
+            AerialDodge1,
+            AerialDodge2,
+            Glide1,
+            Glide2,
+            DodgeRoll1,
+            DodgeRoll2,
         ]
         growths = SeedModifier.starting_growth(StartingMovementOption.LEVEL_2)
         self.assertCountEqual(expected, growths)
 
     def test_movement_level_3(self):
         expected = [
-            HighJump1, HighJump2, HighJump3,
-            QuickRun1, QuickRun2, QuickRun3,
-            AerialDodge1, AerialDodge2, AerialDodge3,
-            Glide1, Glide2, Glide3,
-            DodgeRoll1, DodgeRoll2, DodgeRoll3,
+            HighJump1,
+            HighJump2,
+            HighJump3,
+            QuickRun1,
+            QuickRun2,
+            QuickRun3,
+            AerialDodge1,
+            AerialDodge2,
+            AerialDodge3,
+            Glide1,
+            Glide2,
+            Glide3,
+            DodgeRoll1,
+            DodgeRoll2,
+            DodgeRoll3,
         ]
         growths = SeedModifier.starting_growth(StartingMovementOption.LEVEL_3)
         self.assertCountEqual(expected, growths)
 
     def test_movement_level_4(self):
         expected = [
-            HighJump1, HighJump2, HighJump3, HighJumpMax,
-            QuickRun1, QuickRun2, QuickRun3, QuickRunMax,
-            AerialDodge1, AerialDodge2, AerialDodge3, AerialDodgeMax,
-            Glide1, Glide2, Glide3, GlideMax,
-            DodgeRoll1, DodgeRoll2, DodgeRoll3, DodgeRollMax,
+            HighJump1,
+            HighJump2,
+            HighJump3,
+            HighJumpMax,
+            QuickRun1,
+            QuickRun2,
+            QuickRun3,
+            QuickRunMax,
+            AerialDodge1,
+            AerialDodge2,
+            AerialDodge3,
+            AerialDodgeMax,
+            Glide1,
+            Glide2,
+            Glide3,
+            GlideMax,
+            DodgeRoll1,
+            DodgeRoll2,
+            DodgeRoll3,
+            DodgeRollMax,
         ]
         growths = SeedModifier.starting_growth(StartingMovementOption.LEVEL_4)
         self.assertCountEqual(expected, growths)
@@ -123,5 +203,5 @@ class Tests(unittest.TestCase):
             self.assertEqual(5, len(growths))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
