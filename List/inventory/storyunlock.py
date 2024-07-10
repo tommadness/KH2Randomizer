c81cb0e3d18dfa226a4f7eb3971f7f7eb3b58bf2
equations19 <paulblessing@gmail.com>
2024-07-10 15:06:40 -0500

    Update Disney Castle Key to Royal Summons

--- List/inventory/storyunlock.py
+++ List/inventory/storyunlock.py
@@ -29,9 +29,7 @@ IceCream = StoryUnlock(375, "Ice Cream", locationType.TT, visit_count=3)
 # TODO(zaktherobot): leaving out picture for now, may revisit if there's no confusion that picture doesn't unlock TT2 anymore
 # Picture = StoryUnlock(376, "Picture")
 NaminesSketches = StoryUnlock(368, "Namine's Sketches", locationType.STT, visit_count=1)
-# Note: The display name is kept as "Disney Castle Key" for tracker interop purposes.
-#   We have a hack in place to change the name to "Royal Summons" for the spoiler log for now in itemClass.py.
-RoyalSummons = StoryUnlock(460, "Disney Castle Key", locationType.DC, visit_count=2)  # Dummy 13
+RoyalSummons = StoryUnlock(460, "Royal Summons", locationType.DC, visit_count=2)  # Dummy 13
 
 
 def all_story_unlocks() -> list[StoryUnlock]:
