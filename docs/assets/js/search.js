f27b5cc6448a89b22412633f2169b4c27e5c8ce3
roromaniac <rando4ukraine@gmail.com>
2024-12-24 20:40:27 -0500

    avoid content flashes

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -3,7 +3,13 @@ var lunrIndex;  // Lunr index
 var pagesIndex; // JSON-loaded pages
 // Search result weave: https://liveweave.com/t6I3lh
 
-function getScoreColor(score, maxScore=5) {
+/**
+ * Calculates the color based on the score for visual representation.
+ * @param {number} score - The score to determine the color.
+ * @param {number} [maxScore=5] - The maximum score for a perfect match.
+ * @returns {string} The color in RGB format.
+ */
+function getScoreColor(score, maxScore = 5) {
   // max score determines the score at which a match is considered "perfect" visually
   const normalized = Math.min(Math.max(score / maxScore, 0), 1);
 
@@ -17,6 +23,15 @@ function getScoreColor(score, maxScore=5) {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
+/**
+ * Formats the search result for display.
+ * @param {string} url - The URL of the page.
+ * @param {string} title - The title of the page.
+ * @param {string} matchedContent - The content that matched the search.
+ * @param {number} score - The score of the match.
+ * @param {string} displayedContent - The content to display.
+ * @returns {string} The formatted HTML for the search result.
+ */
 function formatSubmittedSearchResult(url, title, matchedContent, score, displayedContent) {
   
   let snippet;
@@ -48,8 +63,10 @@ function formatSubmittedSearchResult(url, title, matchedContent, score, displaye
 }
 
 
-// Fetch the JSON index
-// USE OF A PROMISE??????
+/**
+ * Fetches the JSON index and builds the Lunr index.
+ * @returns {Promise} A promise that resolves when the index is built.
+ */
 function fetchAndBuildIndex() {
   return fetch('/search.json')
     .then(response => response.json())
@@ -67,9 +84,16 @@ function fetchAndBuildIndex() {
       });
     });
 }
-fetchAndBuildIndex();
 
-function performSearch(query, fuzzySearch, matchedCharsBefore=50, matchedCharsAfter=100) {
+/**
+ * Performs the search based on the query and fuzzy search settings.
+ * @param {string} query - The search query.
+ * @param {number} fuzzySearch - The fuzzy search setting.
+ * @param {number} [matchedCharsBefore=50] - The number of characters to display before the match.
+ * @param {number} [matchedCharsAfter=100] - The number of characters to display after the match.
+ * @returns {Array} An array of search results.
+ */
+function performSearch(query, fuzzySearch, matchedCharsBefore = 50, matchedCharsAfter = 100) {
   if (query.length > 2) { 
     if (lunrIndex === null) {
       fetchAndBuildIndex();
@@ -81,19 +105,26 @@ function performSearch(query, fuzzySearch, matchedCharsBefore=50, matchedCharsAf
         const matchedPage = pagesIndex.find(page => page.url === result.ref);
         // get the matched term names
         const matches = Object.keys(result.matchData.metadata);
-        const firstMatchedTerm = matches[0]
+        const firstMatchedTerm = matches[0];
         const matchedStartIndex = matchedPage.content.indexOf(firstMatchedTerm);
-        return { ...matchedPage, 
+        return { 
+          ...matchedPage, 
           score: result.score, 
           matchedText: firstMatchedTerm,
-          displayedMatchText: matchedPage.content.slice(matchedStartIndex - matchedCharsBefore, matchedStartIndex + matchedCharsAfter)};
+          displayedMatchText: matchedPage.content.slice(matchedStartIndex - matchedCharsBefore, matchedStartIndex + matchedCharsAfter)
+        };
       });
     }
   }
   return []; // Return an empty array if query length is not more than 2
 }
 
-function populateSearch(query, type="menu") {
+/**
+ * Populates the search results based on the query and type.
+ * @param {string} query - The search query.
+ * @param {string} [type="menu"] - The type of search to perform.
+ */
+function populateSearch(query, type = "menu") {
 
   const resultsContainer = document.querySelector(
     type == "menu" ? "#search-results" : "#submitted-search-results"
@@ -106,27 +137,26 @@ function populateSearch(query, type="menu") {
 
   // Clear old results and perform the search
   resultsContainer.innerHTML = '';
-  var results = performSearch(query, fuzzySearch);
-    
-  // handle no results 
-  if (type == "menu") {
+  fetchAndBuildIndex().then(() => {
+    const results = performSearch(query, fuzzySearch);
     if (results.length === 0) {
-      resultsContainer.innerHTML += `<li><a>No results found.</a></li>`;
-    } else { // populate #search-results in a similar way
-      results.forEach(result => {
-        resultsContainer.innerHTML += `<li><a href="${result.url}">${result.title}</a></li>`;
-      });
+        resultsContainer.innerHTML += `<li><a>No results found.</a></li>`;
+    } else {
+        results.forEach(result => {
+            if (type === "menu") {
+                resultsContainer.innerHTML += `<li><a href="${result.url}">${result.title}</a></li>`;
+            } else {
+                resultsContainer.innerHTML += formatSubmittedSearchResult(
+                    result.url,
+                    result.title,
+                    result.matchedText,
+                    result.score,
+                    result.displayedMatchText
+                );
+            }
+        });
     }
-  }
-  if (type == "page") {
-  if (results.length === 0) {
-      resultsContainer.innerHTML += `<li><a>No results found.</a></li>`;
-    } else { // populate #search-results in a similar way
-      results.forEach(result => {
-        resultsContainer.innerHTML += formatSubmittedSearchResult(result.url, result.title, result.matchedText, result.score, result.displayedMatchText);
-      });
-    }
-  }
+  });
 }
 
 // Listen for input changes
@@ -154,29 +184,40 @@ document.querySelector('.search-input').addEventListener('keydown', function(e) 
   }
 })
 
+// this code will overwrite the main-content display from "none" to "block" 
+// with either content or search results depending on the setting we're in
+// this helps with avoiding flashes when hitting back and forward
 document.addEventListener('DOMContentLoaded', function () {
   const urlParams = new URLSearchParams(window.location.search);
   const searchQuery = urlParams.get('search');
+  const mainContent = document.querySelector('.main-content');
+
   if (searchQuery) {
-
-    // clear main content and only show results
-    const mainContent = document.querySelector('.main-content');
-    mainContent.innerHTML = '<div id="submitted-search-results"></div>';
-
-    populateSearch(decodeURIComponent(searchQuery), type = "page");
+      // Replace default content with search results
+      mainContent.innerHTML = '<div id="submitted-search-results"></div>';
+      populateSearch(decodeURIComponent(searchQuery), "page");
   }
+
+  // Show the content after processing
+  mainContent.style.display = 'block';
 });
 
-// Handle the popstate event
 window.addEventListener('popstate', function (event) {
   const state = event.state;
-  // retrieve the search query
-  const searchQuery = state ? state.query : null; 
-  
+  const searchQuery = state ? state.query : null;
+  const mainContent = document.querySelector('.main-content');
+
   if (searchQuery) {
-    // clear main content and populate results
-    const mainContent = document.querySelector('.main-content');
-    mainContent.innerHTML = '<div id="submitted-search-results"></div>';
-    populateSearch(decodeURIComponent(searchQuery), type = "page");
+      // Replace default content with search results
+      mainContent.innerHTML = '<div id="submitted-search-results"></div>';
+      populateSearch(decodeURIComponent(searchQuery), "page");
+  } else {
+      // Restore default content
+      mainContent.innerHTML = `{{ content }}`;
   }
+  mainContent.style.display = 'block';
 });
+
+
+
+
